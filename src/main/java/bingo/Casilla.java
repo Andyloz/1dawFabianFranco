d0e91b1a4ccf461a83ad41a91ed5e0cc98531cb7
lozandyloz <lozandyloz@gmail.com>
2020-04-07 23:43:46 +0200

    Atributo rellenable de Casilla eliminado.

--- src/main/java/bingo/Casilla.java
+++ src/main/java/bingo/Casilla.java
@@ -13,7 +13,6 @@ public class Casilla {
     
     private int num;
     private boolean tachado;
-    private boolean rellenable;
 
     public Casilla() {
     }
@@ -32,13 +31,5 @@ public class Casilla {
 
     public void setTachado(boolean tachado) {
         this.tachado = tachado;
-    }
-
-    public boolean isRellenable() {
-        return rellenable;
-    }
-
-    public void setRellenable(boolean rellenable) {
-        this.rellenable = rellenable;
     }
 }

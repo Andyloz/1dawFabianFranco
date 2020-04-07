43fa5764cb8b4ede5a084535f77224558031b706
lozandyloz <lozandyloz@gmail.com>
2020-04-07 23:09:11 +0200

    Implementación del método generarNumeros()

--- src/main/java/bingo/Carton.java
+++ src/main/java/bingo/Carton.java
@@ -5,6 +5,8 @@
  */
 package bingo;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 /**
@@ -35,6 +37,7 @@ public class Carton {
         }
         
         boolean[][] gridCombinaciones = generarCombinacion();
+        ArrayList<Integer>[] gridNums = generarNumeros(gridCombinaciones);
     }
     
     private boolean[][] generarCombinacion() {
@@ -47,6 +50,53 @@ public class Carton {
             gridCombinaciones[i] = combCols[random.nextInt(combCols.length)];
         }
         return gridCombinaciones;
+    }
+    
+    private ArrayList<Integer>[] generarNumeros(boolean[][] gridCombinaciones) {
+        Random random = new Random();
+        // Almacenaremos los números generados en una variable auxiliar
+        ArrayList<Integer>[] gridNums = new ArrayList[gridCasillas.length];
+        
+        // Recorremos las columnas ->
+        for (int i = 0; i < gridNums.length-1; i++) {
+            // Instanciamos la lista para la columna i
+            gridNums[i] = new ArrayList<>();
+            
+            // Ahora recorremos la fila de posibilidades para esa columna
+            for (boolean combinacion : gridCombinaciones[i]) {
+                // Y si encontramos un true
+                if (combinacion) {
+                    // Añadimos un número aleatorio a la lista de números para esa columna
+                    switch (i) {
+                        // De las columnas 0 a 7 ->
+                        case 0: case 1: case 2: case 3: 
+                        case 4: case 5: case 6: case 7:
+                            gridNums[i].add(Casilla.generarNumero(i));
+                            break;
+                        // Para la última columna ->
+                        case 8:
+                            gridNums[i].add(Casilla.generarNumero(80,99));
+                            break;
+                    }
+                }
+            }
+            
+            // Una vez generados los números:
+            //     Si se han generado dos números para la columna i
+            while (gridNums[i].size() == 2 &&
+                    // Comprobar si los dos números son iguales
+                    gridNums[i].get(0).equals(gridNums[i].get(1))) {
+                gridNums[i].clear();
+                gridNums[i].add(Casilla.generarNumero(i));
+                gridNums[i].add(Casilla.generarNumero(i));
+            // Repetir hasta que los números sean distintos
+            }
+            
+            // Por último ordenar los números en la lista
+            Collections.sort(gridNums[i]);
+        }
+        
+        return gridNums;
     }
 
     public Casilla[][] getCasillas() {

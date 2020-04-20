e38d96e51d00a35756e0447ecb3afa50c2e4fc95
lozandyloz <lozandyloz@gmail.com>
2020-04-20 19:32:12 +0200

    Creación de clase anidada auxiliar en la clase Carton para su asistencia en la generación del cartón.

--- src/main/java/bingo/Carton.java
+++ src/main/java/bingo/Carton.java
@@ -16,7 +16,40 @@ import java.util.Random;
 public class Carton {
     private Casilla[][] gridCasillas;
     
-    
+    private final static class ConstructorCarton {
+        
+        private Casilla[][] gridCasillas;
+
+        private boolean[][] gridCombs;
+        private ArrayList<Integer> filsDisp;
+        private ArrayList<Integer> colsDisp;
+        
+        private int[] numCasPorCol;
+
+        public ConstructorCarton() {
+            this.gridCasillas = new Casilla[3][9];
+
+            // Esta variable marca las casillas a las que se les va a asignar un
+            // número
+            this.gridCombs = new boolean[3][9];
+
+            // Variable de filas disponibles (véase this.comprobarFilsDisp(int))
+            this.filsDisp = new ArrayList<>(3);
+            this.filsDisp.add(0);
+            this.filsDisp.add(1);
+            this.filsDisp.add(2);
+
+            // Variable de columnas disponibles (véase this.comprobarColsDisp(int))
+            this.colsDisp = new ArrayList<>(9);
+            this.colsDisp.add(0); this.colsDisp.add(1); this.colsDisp.add(2);
+            this.colsDisp.add(3); this.colsDisp.add(4); this.colsDisp.add(5);
+            this.colsDisp.add(6); this.colsDisp.add(7); this.colsDisp.add(8);
+            
+            // Esta variable almacenará el número de casillas asignadas por columna.
+            this.numCasPorCol = new int[9];
+        }
+        
+    }
 
     @Override
     public String toString() {

4be0c772b7368014f6544ff0f2370211662f5892
lozandyloz <lozandyloz@gmail.com>
2020-04-21 10:59:17 +0200

    Modularización del método tacharCasilla()

---
+++ src/main/java/bingo/Carton2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bingo;
+
+/**
+ *
+ * @author andyloz
+ */
+public class Carton2 {
+
+    public Carton2() {
+    }
+
+    
+}

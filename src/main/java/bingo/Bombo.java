d82b4ad5b9012dd77c31666ab29a2a5833d7485c
lozandyloz <lozandyloz@gmail.com>
2020-05-20 19:50:38 +0200

    Implementa clase Bombo

--- src/main/java/bingo/Bombo.java
+++ src/main/java/bingo/Bombo.java
@@ -5,10 +5,36 @@
  */
 package bingo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author andyloz
  */
 public abstract class Bombo {
+    private List<Integer> listaBolas;
+
+    public Bombo() {
+        listaBolas = new ArrayList<>();
+    }
     
+    public int sacarBola() {
+        // Devolver el último número de la lista si hay bolas disponibles
+        if (listaBolas.size() > 0) {
+            return listaBolas.get(listaBolas.size() - 1);
+        }
+        // Si no, devolver un número negativo
+        return -1;
+    }
+    
+    public abstract void llenarBombo();
+    
+    public int bolasDentro() {
+        return listaBolas.size();
+    }
+    
+    public boolean vacio() {
+        return listaBolas.isEmpty();
+    }
 }

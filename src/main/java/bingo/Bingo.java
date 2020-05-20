98196462e168abde89375979ff027da8c9c3dea0
lozandyloz <lozandyloz@gmail.com>
2020-05-20 19:43:17 +0200

    Implementa clase Bingo

--- src/main/java/bingo/Bingo.java
+++ src/main/java/bingo/Bingo.java
@@ -5,10 +5,49 @@
  */
 package bingo;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author andyloz
  */
 public abstract class Bingo {
-    
+    private String id;
+    private LocalDate fecha;
+    private String idJugador;
+
+    public Bingo(String id, LocalDate fecha, String idJugador) {
+        this.id = id;
+        this.fecha = fecha;
+        this.idJugador = idJugador;
+    }
+
+    public String getIdJugador() {
+        return idJugador;
+    }
+
+    public void setIdJugador(String idJugador) {
+        this.idJugador = idJugador;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Bingo{" + "id=" + id + ", fecha=" + fecha + ", idJugador=" + idJugador + '}';
+    }
 }

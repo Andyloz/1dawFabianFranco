5bcd322c5cacf8b2667057ef236173b276df616c
lozandyloz <lozandyloz@gmail.com>
2020-05-23 15:51:25 +0200

    Implementa clase BomboAmericano

--- src/main/java/bingo/bombo/BomboAmericano.java
+++ src/main/java/bingo/bombo/BomboAmericano.java
@@ -5,10 +5,32 @@
  */
 package bingo.bombo;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  *
  * @author andyloz
  */
 public final class BomboAmericano extends Bombo {
+    public static final int CANTIDAD_BOLAS = 75;
 
+    public BomboAmericano() {
+        super();
+    }
+
+    @Override
+    public void llenarBombo() {
+        List<Integer> bolas = this.getListaBolas();
+        bolas.clear();
+        
+        bolas.addAll(
+                IntStream.rangeClosed(1, CANTIDAD_BOLAS)
+                        .boxed()
+                        .collect(Collectors.toList()) );
+        
+        Collections.shuffle(bolas);
+    }
 }

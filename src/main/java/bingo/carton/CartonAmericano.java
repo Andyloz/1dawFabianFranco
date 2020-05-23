ff817ba791cb6e0353d60877b952aee3eb381648
lozandyloz <lozandyloz@gmail.com>
2020-05-23 15:57:01 +0200

    Implementa métodos generarCarton y generarNumeros en clase CartonAmericano

--- src/main/java/bingo/carton/CartonAmericano.java
+++ src/main/java/bingo/carton/CartonAmericano.java
@@ -5,6 +5,9 @@
  */
 package bingo.carton;
 
+import java.util.HashSet;
+import java.util.Random;
+
 /**
  *
  * @author andyloz
@@ -16,5 +19,34 @@ public final class CartonAmericano extends Carton {
 
     public CartonAmericano() {
         super(FILAS, COLUMNAS);
+    }
+
+    @Override
+    public void generarCarton() {
+        HashSet<Integer>[] numeros = new HashSet[COLUMNAS];
+        
+        for (int i = 0; i < COLUMNAS; i++) {
+            numeros[i] = new HashSet<>();
+        }
+        
+        generarNumeros(numeros);
+    }
+    
+    
+    private void generarNumeros(HashSet<Integer>[] numeros) {
+        Random random = new Random();
+        
+        for (int i = 0; i < COLUMNAS; i++) {
+            int rangoOrigen = 15 * i;
+            int limiteRango = 16;
+            
+            while (numeros[i].size() != 5) {
+                numeros[i].add(random.nextInt(limiteRango) + rangoOrigen);
+                /**
+                 * Si el número generado ya está presente en el Set, no será
+                 * añadido
+                **/
+            }
+        }
     }
 }

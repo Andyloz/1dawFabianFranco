d326b7a8a0d540c78a7a3c6ed4adb4df928bdcce
lozandyloz <lozandyloz@gmail.com>
2020-05-25 17:20:41 +0200

    Implementa método últimosNumeros en clase CartonEuropeo

--- src/main/java/bingo/carton/CartonEuropeo.java
+++ src/main/java/bingo/carton/CartonEuropeo.java
@@ -49,6 +49,7 @@ public final class CartonEuropeo extends Carton {
         
         
         this.primerosNumeros(bolas, numeros);
+        this.ultimosNumeros(bolas, numeros);
     }
     
     private void primerosNumeros(List<Integer> bolas, List<Integer>[] numeros) {
@@ -79,6 +80,39 @@ public final class CartonEuropeo extends Carton {
                 bolas.remove(i);
                 
                 colRellenas++;
+            }
+        }
+    }
+    
+    private void ultimosNumeros(List<Integer> bolas, List<Integer>[] numeros) {
+        int numsRellenos = 0; // de 6
+        
+        /**
+         * Iteramos sobre la lista de bolas hasta que hayan en las listas
+         * exactamente 15 números.
+         * Es decir, añadimos 6 más a los que ya estaban
+         */
+        for (int i = 0; numsRellenos < 6; i++) {
+            int num = bolas.get(i);
+            int decimas = num / 10;
+            
+            decimas = decimas == 9 // el 90 va en la lista de índice 8
+                    ? 8
+                    : decimas;
+            /**
+             * No pueden haber más de dos números en una misma columna, por lo
+             * que tampoco lo habrá en una lista
+             */
+            if (numeros[decimas].size() != 2) {
+                
+                // Añadimos el número a la lista correspondiente
+                numeros[decimas].add(num);
+                
+                // Eliminamos el número de la lista de bolas para que no vuelva
+                // a aparecer
+                bolas.remove(i);
+                
+                numsRellenos++;
             }
         }
     }

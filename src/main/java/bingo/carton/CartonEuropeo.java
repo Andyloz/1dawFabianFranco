c77a6427da984606c8e6c206bb4b928d98a59e93
lozandyloz <lozandyloz@gmail.com>
2020-05-25 17:32:50 +0200

    Quita main de prueba en clase CartonEuropeo

--- src/main/java/bingo/carton/CartonEuropeo.java
+++ src/main/java/bingo/carton/CartonEuropeo.java
@@ -196,10 +196,4 @@ public final class CartonEuropeo extends Carton {
         
         return matriz;
     }
-    
-    public static void main(String[] args) {
-        for (int i = 0; i < 1; i++) {
-            new CartonEuropeo();
-        }
-    }
 }

19fb77cf9dd3548b77ae8d37838ade3e1840c675
lozandyloz <lozandyloz@gmail.com>
2020-05-25 17:23:58 +0200

    Implementa método colocarNumeros en clase CartonEuropeo

--- src/main/java/bingo/carton/CartonEuropeo.java
+++ src/main/java/bingo/carton/CartonEuropeo.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -50,6 +51,12 @@ public final class CartonEuropeo extends Carton {
         
         this.primerosNumeros(bolas, numeros);
         this.ultimosNumeros(bolas, numeros);
+        
+        
+        int[][] matriz = null;
+        while (matriz == null) {            
+            matriz = this.colocarNumeros(numeros);
+        }
     }
     
     private void primerosNumeros(List<Integer> bolas, List<Integer>[] numeros) {
@@ -114,6 +121,83 @@ public final class CartonEuropeo extends Carton {
                 
                 numsRellenos++;
             }
+        }
+    }
+
+    private int[][] colocarNumeros(List<Integer>[] numeros) {
+        int[][] matriz = new int[FILAS][COLUMNAS];
+        int[] cantNumsPorFilas = new int[FILAS];
+        
+        // Se colocarán los números por columnas
+        for (int col = 0; col < COLUMNAS; col++) {
+            /**
+             * Esta variable se encargará de almacenar los índices de las filas
+             * disponibles.
+             * A lo largo del bucle se irán descartando las filas de la columna
+             * que no podrán ser rellenadas
+             */
+            List<Integer> filasDisp = new ArrayList<>(Arrays.asList(0, 1, 2));
+            
+            // Si alguna de las filas tienen ya 5 números (el máx. por fila)...
+            if (col > 6) {
+                for (int fil = 0; fil < FILAS; fil++) {
+                    if (cantNumsPorFilas[fil] == 5) {
+                        filasDisp.remove((Integer) fil); // ...descartada
+                    }
+                }
+            }
+            
+            if (col > 1) {                
+                for (int fil = 0; fil < FILAS; fil++) {
+                    // Si las anteriores dos casillas a la casilla en [fil][col]
+                    // están rellenas...
+                    if (matriz[fil][col-1] != 0 && matriz[fil][col-2] != 0) {
+                        filasDisp.remove((Integer) fil); // ...descartada
+                    }
+                }
+            }
+            
+            /**
+             * Si no hay suficientes filas disponibles para la cantidad de
+             * números generados para esa columna, se habrá caído en una 
+             * combinación imposible, por lo que no devolerá la matriz
+             */
+            if (filasDisp.size() >= numeros[col].size()) {
+                
+                // Si sobran filas, habrá que descartar alguna aleatoriamente
+                Random random = new Random();
+                
+                while (filasDisp.size() != numeros[col].size()) {                    
+                    filasDisp.remove(
+                            random.nextInt(filasDisp.size()));
+                }
+                
+                /**
+                 * Se irán insertando los números generados ordenados en las
+                 * filas que indica la lista de filasDisponibles
+                 */
+                Collections.sort(numeros[col]);
+                
+                for (int i = 0; i < filasDisp.size(); i++) {
+                    int fil = filasDisp.get(i);
+                    int numero = numeros[col].get(i);
+                    
+                    matriz[fil][col] = numero;
+                    
+                    // Se controlará la cantidad de números insertados por fila
+                    cantNumsPorFilas[fil]++;
+                }
+            } else {
+                return null;
+            }
+        }
+        
+        return matriz;
+    }
+    
+    public static void main(String[] args) {
+        for (int i = 0; i < 1; i++) {
+            new CartonEuropeo();
         }
     }
 }

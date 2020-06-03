ace596aa7dd8e6033468e4802ce02ee9409447e4
andyloz <lozandyloz@gmail.com>
2020-06-03 12:50:08 +0200

    Implementa nueva clase BingoMysqlCon

---
+++ src/main/java/bingo/dao/BingoMysqlCon.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bingo.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author andyloz
+ */
+public class BingoMysqlCon {
+    private static Connection conexion = null;
+    
+    private static final String SERVIDOR = "jdbc:mysql://localhost/";
+    private static final String BD = "bingo";
+    private static final String USER = "bingo";
+    private static final String PASS = "bingo";
+    //private static final String PARAMS;
+
+    private BingoMysqlCon() {
+    }
+    
+    public static Connection getInstance() throws SQLException {
+        
+        if (conexion == null) {
+            conexion = DriverManager.getConnection(SERVIDOR + BD, USER, PASS);
+        }
+        
+        return conexion;
+    }
+}

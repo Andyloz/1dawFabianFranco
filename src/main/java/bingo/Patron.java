7757c846abdb321652c4581ce396365aa1e0e83c
andyloz <lozandyloz@gmail.com>
2020-06-11 16:03:38 +0200

    Añade patrones al enum Patron

--- src/main/java/bingo/Patron.java
+++ src/main/java/bingo/Patron.java
@@ -14,12 +14,33 @@ import java.util.List;
  */
 public enum Patron {
     
-    COMPLETO("Carton completo",
+    COMPLETO("Carton casi completo",
+            new Point(0,0), new Point(0,1), new Point(0,2), new Point(0,3), new Point(0,4),
+            new Point(1,0), new Point(1,1), new Point(1,2), new Point(1,3), new Point(1,4),
+            new Point(2,0), new Point(2,1), /*************/ new Point(2,3), new Point(2,4),
+            new Point(3,0), new Point(3,1), new Point(3,2), new Point(3,3), new Point(3,4),
+            new Point(4,0), new Point(4,1), new Point(4,2), new Point(4,3), new Point(4,4)),
+    
+    DIAMANTE("Carton completo",
+            /*************/ /*************/ new Point(0,2), /*************/ /*************/
+            /*************/ new Point(1,1), new Point(1,2), new Point(1,3), /*************/
+            new Point(2,0), new Point(2,1), new Point(2,1), new Point(2,3), new Point(2,4),
+            /*************/ new Point(3,1), new Point(3,2), new Point(3,3), /*************/
+            /*************/ /*************/ new Point(4,2)  /*************/ /*************/),
+    
+    DIAGONAL_1("Carton completo",
+            /*************/ new Point(0,1), new Point(0,2), new Point(0,3), new Point(0,4),
+            new Point(1,0), /*************/ new Point(1,2), new Point(1,3), new Point(1,4),
+            new Point(2,0), new Point(2,1), /*************/ new Point(2,3), new Point(2,4),
+            new Point(3,0), new Point(3,1), new Point(3,2), /*************/ new Point(3,4),
+            new Point(4,0), new Point(4,1), new Point(4,2), new Point(4,3)  /*************/),
+    
+    DIAGONAL_2("Carton completo",
             new Point(0,0), new Point(0,1), new Point(0,2), new Point(0,3), /*************/
-            new Point(1,0), /*************/ /*************/ new Point(1,3), /*************/
-            new Point(2,0), /*************/ new Point(2,2), new Point(2,3), new Point(2,4),
-            new Point(3,0), /*************/ /*************/ new Point(3,3), /*************/
-            new Point(4,0), /*************/ new Point(4,2), /*************/ new Point(4,4)),
+            new Point(1,0), new Point(1,1), new Point(1,2), /*************/ new Point(1,4),
+            new Point(2,0), new Point(2,1), /*************/ new Point(2,3), new Point(2,4),
+            new Point(3,0), /*************/ new Point(3,2), new Point(3,3), new Point(3,4),
+            /*************/ new Point(4,1), new Point(4,2), new Point(4,3), new Point(4,4)),
     
     AHORCADO("El tradicional ahorcado",
             new Point(0,0), new Point(0,1), new Point(0,2), new Point(0,3), /*************/
@@ -35,12 +56,19 @@ public enum Patron {
             /*************/ /*************/ new Point(3,2), /*************/ /*************/
             new Point(4,0), /*************/ /*************/ /*************/ new Point(4,4)),
     
-    X("La letra X",
+    LETRA_X("La letra X",
             new Point(0,0), /*************/ /*************/ /*************/ new Point(0,4),
             /*************/ new Point(1,1), /*************/ new Point(1,3), /*************/
             /*************/ /*************/ new Point(2,2), /*************/ /*************/
             /*************/ new Point(3,1), /*************/ new Point(3,3), /*************/
-            new Point(4,0), /*************/ /*************/ /*************/ new Point(4,4));
+            new Point(4,0), /*************/ /*************/ /*************/ new Point(4,4)),
+    
+    LETRA_Z("La letra X",
+            /*************/ /*************/ /*************/ /*************/ /*************/
+            new Point(1,0), new Point(1,1), new Point(1,2), /*************/ new Point(1,4),
+            new Point(2,0), new Point(2,1), /*************/ new Point(2,3), new Point(2,4),
+            new Point(3,0), /*************/ new Point(3,2), new Point(3,3), new Point(3,4)
+            /*************/ /*************/ /*************/ /*************/ /*************/);
     
     
     private final String descripcion;

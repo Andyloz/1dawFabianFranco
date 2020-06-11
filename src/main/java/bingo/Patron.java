66475b40250591fa14a5bdafa89c9e1ff4cf0bb5
andyloz <lozandyloz@gmail.com>
2020-06-11 16:09:26 +0200

    Corrije pequeño error en clase Patron

--- src/main/java/bingo/Patron.java
+++ src/main/java/bingo/Patron.java
@@ -24,7 +24,7 @@ public enum Patron {
     DIAMANTE("Carton completo",
             /*************/ /*************/ new Point(0,2), /*************/ /*************/
             /*************/ new Point(1,1), new Point(1,2), new Point(1,3), /*************/
-            new Point(2,0), new Point(2,1), new Point(2,1), new Point(2,3), new Point(2,4),
+            new Point(2,0), new Point(2,1), new Point(2,2), new Point(2,3), new Point(2,4),
             /*************/ new Point(3,1), new Point(3,2), new Point(3,3), /*************/
             /*************/ /*************/ new Point(4,2)  /*************/ /*************/),
     
@@ -57,11 +57,11 @@ public enum Patron {
             new Point(4,0), /*************/ /*************/ /*************/ new Point(4,4)),
     
     LETRA_X("La letra X",
-            new Point(0,0), /*************/ /*************/ /*************/ new Point(0,4),
-            /*************/ new Point(1,1), /*************/ new Point(1,3), /*************/
-            /*************/ /*************/ new Point(2,2), /*************/ /*************/
-            /*************/ new Point(3,1), /*************/ new Point(3,3), /*************/
-            new Point(4,0), /*************/ /*************/ /*************/ new Point(4,4)),
+            /*************/ new Point(0,1), new Point(0,2), new Point(0,3), /*************/
+            new Point(1,0), /*************/ new Point(1,2), /*************/ new Point(1,4),
+            new Point(2,0), new Point(2,1), /*************/ new Point(2,3), new Point(2,4),
+            new Point(3,0), /*************/ new Point(3,2), /*************/ new Point(3,4),
+            /*************/ new Point(4,1), new Point(4,2), new Point(4,3)  /*************/),
     
     LETRA_Z("La letra X",
             /*************/ /*************/ /*************/ /*************/ /*************/
